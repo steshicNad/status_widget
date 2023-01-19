c178c456efc721e382ecc23a91d7bc14dcce31bf
nadya <nadyasteshits@yandex.ru>
2023-01-19 18:22:28 +0300

    final commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="flex flex-col w-[25.9vw] rounded-2xl h-[34.8vh] bg-[#30323D]">
     <div
-      class="flex z-10 w-[25.9vw] rounded-t-2xl h-[3.8vh] gap-[0.83vw] bg-[#30323D] shadow-md shadow-[#272933]"
-    >
-      <div class="flex ml-[1.04vw] items-center text-[1.6vh] uppercase text-[#00FF94]"
-        >Состояния</div
-      >
+      class="flex z-10 w-[25.9vw] rounded-t-2xl h-[3.8vh] gap-[0.83vw] bg-[#30323D] shadow-md shadow-[#272933]">
+      <div class="flex ml-[1.04vw] items-center text-[1.6vh] uppercase text-[#00FF94]">
+        Состояния
+      </div>
       <div class="ml-[7.34vw] w-maxw flex items-center gap-[0.83vw]">
         <div class="flex items-center text-[0.92vh] text-[#D6EBFF]">Время начала</div>
         <div class="flex items-center">
@@ -14,18 +13,24 @@
         <div class="flex items-center text-[0.92vh] text-[#D6EBFF]">Время окончания</div>
       </div>
     </div>
-    <div class="flex flex-col mt-[2.4vh] rounded-b-2xl w-[25.9vw] h-[31vh] bg-[#30323D]">
+    <div
+      class="flex flex-col pb-[2.4vh] mt-[2.4vh] overflow-y-auto pb-10 rounded-b-2xl w-[25.9vw] h-[31vh] bg-[#30323D]">
       <!-- 1 строка (таких надо 5) -->
 
-      <div class="flex flex-col ml-[1.04vw]" v-for="i in test" :key="i">
+      <div class="flex flex-col ml-[1.04vw]" v-for="(item, index) in test" :key="item">
         <div class="flex items-center">
-          <div class="flex w-[10vw] items-center text-[1.48vh] text-[#00FF94]">{{
-            i.condition
-          }}</div>
-          <div class="flex w-maxw ml-[2.76vw] gap-[0.83vw] text-[1.1vh] text-[#00FF94]">
+          <div
+            v-if="item.state == 'working'"
+            class="flex truncate w-[10vw] items-center text-[1.48vh] text-[#00FF94]">
+            {{ item.condition }}
+          </div>
+          <div v-else class="flex truncate w-[10vw] items-center text-[1.48vh] text-[#D6EBFF]">
+            {{ item.condition }}
+          </div>
+          <div class="flex ml-[2.76vw] gap-[0.83vw] text-[1.1vh] text-[#00FF94]">
             <div class="flex gap-[0.41vw]">
-              <div class="flex items-center">{{ i.data1 }}</div>
-              <div class="flex items-center">{{ i.time1 }}</div>
+              <div class="flex items-center">{{ item.data1 }}</div>
+              <div class="flex items-center">{{ item.time1 }}</div>
             </div>
 
             <div class="flex items-center">
@@ -33,18 +38,22 @@
             </div>
 
             <div class="flex gap-[0.41vw]">
-              <div class="flex items-center">{{ i.data2 }}</div>
-              <div class="flex items-center">{{ i.time2 }}</div>
+              <div class="flex items-center">{{ item.data2 }}</div>
+              <div class="flex items-center">{{ item.time2 }}</div>
             </div>
           </div>
         </div>
-        <hr class="h-px my-[0.83vh] w-[23.9vw] bg-[#00C2FF] border-0" />
+        <hr
+          v-if="index != datasize - 1"
+          class="h-px my-[0.83vh] w-[23.9vw] bg-[#00C2FF] border-0" />
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+  import { ref } from 'vue';
+
   defineProps({
     msg: {
       type: String,
@@ -52,13 +61,14 @@
     },
   });
 
-  const test = [
+  const test = ref([
     {
       condition: 'В работе',
       data1: '06.10.22',
       time1: '16:49',
       data2: '12.10.22',
       time2: '12:00',
+      state: 'working',
     },
     {
       condition: 'Работа на ТТ',
@@ -66,6 +76,7 @@
       time1: '12:29',
       data2: '--------',
       time2: '',
+      state: 'not working',
     },
     {
       condition: 'Температура норм',
@@ -73,6 +84,7 @@
       time1: '11:55',
       data2: '14.10.22',
       time2: '15:32',
+      state: 'working',
     },
     {
       condition: 'Номинальные обороты',
@@ -80,6 +92,7 @@
       time1: '10:12',
       data2: '15.10.22',
       time2: '10:11',
+      state: 'not working',
     },
     {
       condition: 'Расход топлива норм',
@@ -87,26 +100,63 @@
       time1: '15:25',
       data2: '16.10.22',
       time2: '13:55',
+      state: 'working',
     },
-  ];
+    {
+      condition: 'Номинальные обороты',
+      data1: '09.10.22',
+      time1: '10:12',
+      data2: '15.10.22',
+      time2: '10:11',
+      state: 'not working',
+    },
+    {
+      condition: 'Расход топлива норм',
+      data1: '10.10.22',
+      time1: '15:25',
+      data2: '16.10.22',
+      time2: '13:55',
+      state: 'working',
+    },
+    {
+      condition: 'Номинальные обороты',
+      data1: '09.10.22',
+      time1: '10:12',
+      data2: '15.10.22',
+      time2: '10:11',
+      state: 'not working',
+    },
+    {
+      condition: 'Расход топлива норм',
+      data1: '10.10.22',
+      time1: '15:25',
+      data2: '16.10.22',
+      time2: '13:55',
+      state: 'working',
+    },
+    {
+      condition: 'Номинальные обороты',
+      data1: '09.10.22',
+      time1: '10:12',
+      data2: '15.10.22',
+      time2: '10:11',
+      state: 'not working',
+    },
+    {
+      condition: 'Расход топлива норм',
+      data1: '10.10.22',
+      time1: '15:25',
+      data2: '16.10.22',
+      time2: '13:55',
+      state: 'working',
+    },
+  ]);
+
   const data = {
     test,
   };
-  // data: () => {
-  //   return {
-  //     conditions: [
-  //       'Расход топлива норм',
-  //       'Номинальные обороты',
-  //       'Температура норм',
-  //       'Работа на ТТ',
-  //       'В работе',
-  //     ],
-  //     data1: ['06.10.22', '07.10.22', '08.10.22', '09.10.22', '10.10.22'],
-  //     data2: ['12.10.22', '--------', '14.10.22', '15.10.22', '16.10.22'],
-  //     time1: ['16:49', '12:29', '11:55', '10:12', '15:25'],
-  //     time2: ['12:00', '', '15:32', '10:11', '13:55'],
-  //   };
-  // };
+
+  const datasize = test.value.length;
 </script>
 
 <style scoped></style>
